811a7eb7684a236065b8f6618cd0b82fa458909b
BananaSpritee <164941682+BananaSpritee@users.noreply.github.com>
2025-07-18 15:09:07 -0300

    .

--- CNPJValidatorTest.php
+++ CNPJValidatorTest.php
@@ -4,25 +4,34 @@ use PHPUnit\Framework\TestCase;
 
 require_once 'CNPJValidator.php';
 
-class CNPJValidatorTest extends TestCase
-{
-    public function testIsValid($cnpj, $expected){
+class CNPJValidatorTest extends TestCase{
 
-    $dataProvider = [
+    /**
+    * @dataProvider provideCnpjsParaValidacao
+    */
 
-        "11444777000161" => True,
-        "11111111111111" => False,
+    public function testIsValid(string $cnpj, bool $expectedResult){
 
-    ];
+        $validator = new CNPJValidator();
+        $result = $validator->isValid($cnpj);
 
-        foreach ($dataProvider as $data => $expected){
+        $this->assertSame($expectedResult, $result, "Falha ao validar o CNPJ: {$cnpj}");
 
-            $validator = new CNPJValidator($data);
-            $isValid = $validator(isValid);
-            $this->assertEquals($expected, $validator);
-
-        }
-        
     }
+
+    public static function provideCnpjsParaValidacao(){
+
+        return [
+
+            'CNPJ válido sem máscara' => ['11444777000161', true],
+            'CNPJ válido com máscara' => ['11.444.777/0001-61', true],
+            'CNPJ inválido (todos os dígitos iguais)' => ['11111111111111', false],
+            'CNPJ inválido (dígitos verificadores errados)' => ['11444777000162', false],
+            'CNPJ inválido (comprimento incorreto)' => ['12345', false],
+            'String vazia' => ['', false],
+
+        ];
+
+    }
+
 }
-?>
